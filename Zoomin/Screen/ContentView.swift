d43045322b6e9014f92440cbdd417df4ae9bfd1a
dekapd99 <dekaprimatio@gmail.com>
2023-03-13 06:55:19 +0700

    double tap gesture features implemented

--- Zoomin/Screen/ContentView.swift
+++ Zoomin/Screen/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTY
+    
+    @State private var isAnimating:Bool = false
+    @State private var imageScale: CGFloat = 1
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - CONTENT
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            ZStack {
+                // MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                // MARK: - 1, Tap Gesture
+                    .onTapGesture(count: 2 , perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
+            } //: ZStack
+            .navigationTitle("Start Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 2)) {
+                    isAnimating = true
+                }
+            })
+        } //: NAVIGATION
+        .navigationViewStyle(.stack)
     }
 }
 
