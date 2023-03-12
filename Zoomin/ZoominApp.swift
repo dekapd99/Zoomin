6d6a3ec4c33f7d2e3d3adaa222bfec9597cbdaaf
dekapd99 <dekaprimatio@gmail.com>
2023-03-12 21:38:49 +0700

    Initial Commit

---
+++ Zoomin/ZoominApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoominApp.swift
+//  Zoomin
+//
+//  Created by Deka Primatio on 12/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ZoominApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
