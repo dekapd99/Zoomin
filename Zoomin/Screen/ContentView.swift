b07360359f3c727eac6874987b97c2ab11ce2286
dekapd99 <dekaprimatio@gmail.com>
2023-03-12 22:11:49 +0700

    project folder & files configuration and assets

---
+++ Zoomin/Screen/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Zoomin
+//
+//  Created by Deka Primatio on 12/03/23. deka
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
