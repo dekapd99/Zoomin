f89b53004a8063abc5783bd805c12a18b3c5b08c
dekapd99 <dekaprimatio@gmail.com>
2023-03-14 04:33:31 +0700

    implementasi Drag Gestures

--- Zoomin/Screen/ContentView.swift
+++ Zoomin/Screen/ContentView.swift
@@ -12,8 +12,16 @@ struct ContentView: View {
     
     @State private var isAnimating:Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - FUNCTION
+    // Reset Image Scale & Offset setelah di double klik kembali
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - CONTENT
     
@@ -28,19 +36,35 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
+                
                 // MARK: - 1, Tap Gesture
+                // Jika di Tap 2x, maka akan zoom sebesar 5x dan jika tidak maka kembali ke posisi zoom 1x
                     .onTapGesture(count: 2 , perform: {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                
+                // MARK: - 2, Drag Gesture
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 2)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             } //: ZStack
             .navigationTitle("Start Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
