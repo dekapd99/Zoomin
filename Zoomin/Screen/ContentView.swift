fc6d44502cf19982f89f07a5f5b04aa3566349dc
dekapd99 <dekaprimatio@gmail.com>
2023-03-16 18:58:28 +0700

    implementing magnification gesture & drawer handle

--- Zoomin/Screen/ContentView.swift
+++ Zoomin/Screen/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     @State private var isAnimating:Bool = false
     @State private var imageScale: CGFloat = 1
     @State private var imageOffset: CGSize = .zero
+    @State private var isDrawerOpen: Bool = false
     
     // MARK: - FUNCTION
     // Reset Image Scale & Offset setelah di double klik kembali
@@ -66,6 +67,26 @@ struct ContentView: View {
                                 }
                             }
                     )
+                // MARK: - 3. MAGNIFICATION GESTURE
+                .gesture(
+                MagnificationGesture()
+                    .onChanged { value in
+                        withAnimation(.linear(duration: 2)) {
+                            if imageScale >= 1 && imageScale <= 5 {
+                                imageScale = value
+                            } else if imageScale > 5 {
+                                imageScale = 5
+                            }
+                        }
+                    }
+                    .onEnded { _ in
+                        if imageScale > 5 {
+                            imageScale = 5
+                        } else if imageScale <= 1 {
+                            resetImageState()
+                        }
+                    }
+                )
             } //: ZStack
             .navigationTitle("Start Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
@@ -135,12 +156,40 @@ struct ContentView: View {
                     .padding(.bottom, 30)
                 , alignment: .bottom
             )
-            
+            //MARK: - DRAWER OVERLAY
+            .overlay(
+                HStack(spacing: 12) {
+                    //MARK: - DRAWER HANDLE
+                    Image(systemName: isDrawerOpen ? "chevron.compact.right" : "chevron.compact.left")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 40)
+                        .padding(8)
+                        .foregroundStyle(.secondary)
+                        .onTapGesture(perform: {
+                            withAnimation(.easeOut) {
+                                isDrawerOpen.toggle()
+                            }
+                        })
+                    
+                    //MARK: - THUMBNAIL
+                    Spacer()
+                } //: DRAWER
+                    .padding(EdgeInsets(top: 16, leading: 8, bottom: 16, trailing: 8))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                    .frame(width: 260)
+                    .padding(.top, UIScreen.main.bounds.height / 12)
+                    .offset(x: isDrawerOpen ? 20 : 215)
+                , alignment: .topTrailing
+            )
         } //: NAVIGATION
         .navigationViewStyle(.stack)
     }
 }
 
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
